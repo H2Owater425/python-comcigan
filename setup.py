9fb24b57554cdfc4ba50984ff9029b93d63983a3
h2owater425 <h2o@h2owr.xyz>
2021-10-20 00:15:24 +0900

    feat: Implement comcigan api

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+packages = find_packages()
+print(packages)
+
+setup(
+	name='python-comcigan',
+	version='1.0.0',
+	description='Comcigan api for Python',
+	author='H2Owater425',
+	author_email='h2o@h2owr.xyz',
+	url='https://github.com/H2Owater425/python-comcigan',
+	license='MIT',
+	packages=packages,
+	classifiers=[
+			"Programming Language :: Python :: 3",
+			"License :: OSI Approved :: MIT License",
+			"Operating System :: OS Independent",
+	],
+	python_requires='>=3.7',
+)
