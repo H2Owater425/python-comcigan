b94978668295076ffea36fdc38a9119bbc7fab07
h2owater425 <h2o@h2owr.xyz>
2021-10-20 14:04:34 +0900

    feat: Add encoding on opening long_description

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
-packages = find_packages()
 
 setup(
 	name='python-comcigan',
 	version='1.0.4',
 	description='Comcigan api for Python',
-	long_description=open('./README.md').read(),
+	long_description=open(file='./README.md', mode='r', encoding='UTF-8').read(),
 	long_description_content_type='text/markdown',
 	author='H2Owater425',
 	author_email='h2o@h2owr.xyz',
 	url='https://github.com/H2Owater425/python-comcigan',
 	license='MIT',
-	packages=packages,
+	packages=find_packages(),
 	classifiers=[
 			"Programming Language :: Python :: 3",
 			"License :: OSI Approved :: MIT License",
